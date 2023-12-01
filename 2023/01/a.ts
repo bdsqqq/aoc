afb7a78e0925c7031e08ab31acb101d13992a4ba
Igor Bedesqui <igorbedesqui@gmail.com>
2023-12-01 23:53:21 +0000

    feat: 2023-01-a more compact

--- 2023/01/a.ts
+++ 2023/01/a.ts
@@ -6,17 +6,14 @@ const textInput = await input.text();
 
 const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-const calibrationValues = textInput.split("\n").map((line) => {
-  const chars = line.split("");
-
-  const first = chars.find((char) => digits.includes(char)) + "";
-  const last = chars.findLast((char) => digits.includes(char)) + "";
-
-  return parseInt(first + last);
-});
-
-const totalCalibrationValue = calibrationValues.reduce(
-  (prev, curr) => prev + curr
-);
+const totalCalibrationValue = textInput
+  .split("\n")
+  .map((line) =>
+    parseInt(
+      line.split("").find((char) => digits.includes(char)) +
+        line.split("").findLast((char) => digits.includes(char))!
+    )
+  )
+  .reduce((prev, curr) => prev + curr);
 
 console.log(totalCalibrationValue);
